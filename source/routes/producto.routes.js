3b455bd97ed25a1a7da21f6f421799f577a36117
thecarrot912 <thecarrot911@gmail.com>
2023-03-31 00:33:42 -0300

    Se agregaron las consultas de client thunder

--- source/routes/producto.routes.js
+++ source/routes/producto.routes.js
@@ -7,6 +7,7 @@ const { imagenFormat } = require("../libs/multerConfig");
 // Producto
 router.get("", [authToken.VerificarToken],productoController.mostrarProductos);
 router.post("/agregar",[authToken.VerificarToken,imagenFormat.Producto.single("imagen")],productoController.agregarProducto);
-router.delete("/eliminar/:codigo",[authToken.VerificarToken],productoController.eliminarProducto);
+router.post("/modificar",[authToken.VerificarToken, imagenFormat.Producto.single("imagen")],productoController.modificarProducto);
+//router.delete("/eliminar/:codigo",[authToken.VerificarToken],productoController.eliminarProducto);
 
 module.exports = router;
