bfec86084d4c44baa2f4618739c436a32a1ab5b3
Zerepingas <zeretto4210@live.com>
2023-06-27 20:27:05 -0400

    multer

--- source/routes/libro.routes.js
+++ source/routes/libro.routes.js
@@ -2,8 +2,9 @@ const express = require("express");
 const { authToken } = require("../middlewares/autenticacion.jwt");
 const router = express.Router();
 const { libroController } = require("../app/libro/libro.controller");
+const { multerParser } = require("../libs/multerConfig");
 
 router.get("", [authToken.VerificarToken], libroController.MostrarLibro);
-router.post("/registrar_libro",[authToken.VerificarToken], libroController.RegistrarLibro);
+router.post("/registrar_libro",[authToken.VerificarToken,multerParser.Producto.none()], libroController.RegistrarLibro);
 
 module.exports = router;
