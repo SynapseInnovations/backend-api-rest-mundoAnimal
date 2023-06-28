03b6fbb2d8233959c2637f2f6bf966d4a0228e62
thecarrot912 <thecarrot911@gmail.com>
2023-06-27 20:45:57 -0400

    se agrego el multer al enrutamiento de prestamo

--- source/routes/prestamo.routes.js
+++ source/routes/prestamo.routes.js
@@ -2,8 +2,9 @@ const express = require("express");
 const { authToken } = require("../middlewares/autenticacion.jwt");
 const router = express.Router();
 const { prestamoController } = require("../app/prestamo/prestar.controller");
+const { multerParser } = require("../libs/multerConfig");
 
 router.get("", [authToken.VerificarToken], prestamoController.MostrarPrestamo);
-router.post("/prestar_libro",[authToken.VerificarToken], prestamoController.PrestarLibro);
+router.post("/prestar_libro",[authToken.VerificarToken, multerParser.Producto.none()], prestamoController.PrestarLibro);
 
 module.exports = router;
